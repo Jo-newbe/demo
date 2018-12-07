9e71c3a312bf76a32893eb6608ab2a71fa3cfb53
guoguo <guoguo@hh.com>
2018-12-07 09:40:10 +0800

    添加了路由的功能

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-const http = require('http')
+const http = require("http");
 
 http
-  .createServer((req ,res) => {
-    res.end('Hello, World!')
+  .createServer((req, res) => {
+    switch (req.url) {
+      case "/":
+        res.end("这是主页");
+        break;
+      case "/about":
+        res.end("这是关于页面");
+        break;
+      case "/list":
+        res.end("这是列表页面");
+        break;
+      default:
+        res.end("Hello, World!");
+    }
+    // res.end('Hello, World!')
   })
-  .listen(8080)
-
+  .listen(8080);
